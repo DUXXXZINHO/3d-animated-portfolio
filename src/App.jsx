ff518ba35096fe54860919345b8599a03cde0971
Eduardo Alex <ealex1377@gmail.com>
2026-04-09 19:01:50 +0000

    aula quinta feira

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import Hero from  "./Components/hero/Hero";
+import Services from "./Components/services/Services";
+import Contact from "./Components/contact/Contact";
+import Portifolio from "./Components/portifolio/Portifolio";
+
+
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className="container">
+      <section id="#hero">
+      <Hero/>
+      </section>
+
+      <section id="#services">
+      <Services/>
+      </section>
+
+      <section id="#portifolio">
+      <Portifolio/>
+      </section>
+
+      <section id="#contact">
+      <Contact/>
+      </section>
+    </div>
   )
 }
 
